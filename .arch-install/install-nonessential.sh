e97bfd7da7fabe5b8955a53e473259e128ed4a5e
Ayberk Aydin <e187569@metu.edu.tr>
2020-04-04 16:22:45 +0300

    upd

--- .arch-install/install-nonessential.sh
+++ .arch-install/install-nonessential.sh
@@ -4,11 +4,6 @@ sudo pacman -S redshift  --noconfirm
 sudo pacman -S cmake --noconfirm
 sudo pacman -S python-pip --noconfirm
 sudo pacman -S kolourpaint  --noconfirm
-sudo pacman -S htop --noconfirm
 sudo pacman -S neofetch --noconfirm
-sudo pacman -S unrar --noconfirm
-sudo pacman -S p7zip --noconfirm
-sudo pacman -S cgvfs-smb --noconfirm
 sudo pacman -S nmap  --noconfirm
-sudo pacman -S neovim  --noconfirm
 
