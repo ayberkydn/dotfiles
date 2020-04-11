11068208db7986a9b7d32ca7038c4efaae6c91ec
Ayberk Aydin <e187569@metu.edu.tr>
2020-04-11 15:09:42 +0300

    upd

--- .arch-install/handle-dotfiles.sh
+++ .arch-install/handle-dotfiles.sh
@@ -2,8 +2,8 @@
 ###################################
 #### handle dotfiles ##############
 ###################################
-git clone --separate-git-dir=$HOME/.dotfiles https://github.com/aybberk/dotfiles.git tmpdotfiles
+git clone --separate-git-dir=$HOME/.dotfiles https://github.com/ayberkydn/dotfiles.git tmpdotfiles
 rsync --recursive --verbose --exclude '.git' tmpdotfiles/ $HOME/
 rm -r tmpdotfiles
-git clone --bare https://www.github.com/aybberk/dotfiles.git $HOME/.dotfiles.git 
+git clone --bare https://www.github.com/ayberkydn/dotfiles.git $HOME/.dotfiles.git 
 
