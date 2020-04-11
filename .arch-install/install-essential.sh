0308474d13b029ccdf425f4eede1e7b47221a85b
Ayberk <ayberkydn@gmail.com>
2020-04-11 23:03:50 +0300

    upd

--- .arch-install/install-essential.sh
+++ .arch-install/install-essential.sh
@@ -5,6 +5,6 @@ sudo pacman -S zsh --noconfirm
 ##### OHMYZSH ######
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-sudo pacman-mirrors --fasttrack && sudo pacman -Syyu --noconfirm
+sudo pacman-mirrors --fasttrack ; sudo pacman -Syyu --noconfirm
 
 sudo pacman -S i3 rofi sxhkd zsh yay gvim neovim gtop py3status nitrogen base base-devel termite compton flameshot ttf-font-awesome chromium --noconfirm
