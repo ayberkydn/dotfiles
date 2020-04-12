81e7450e3924e59675d381bf42565df1db82e36f
Ayberk Aydin <e187569@metu.edu.tr>
2020-04-12 22:58:40 +0300

    upd

--- .arch-install/install-nonessential.sh
+++ .arch-install/install-nonessential.sh
@@ -3,7 +3,6 @@ sudo pacman -S noto-fonts-emoji --noconfirm
 sudo pacman -S redshift  --noconfirm
 sudo pacman -S cmake --noconfirm
 sudo pacman -S python-pip --noconfirm
-sudo pacman -S kolourpaint  --noconfirm
 sudo pacman -S neofetch --noconfirm
 sudo pacman -S nmap  --noconfirm
 
