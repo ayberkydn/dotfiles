4da3495ea56de66eaf8db1cc55760f4279d888d5
Ayberk Aydin <e187569@metu.edu.tr>
2020-04-29 05:25:36 +0300

    upd

--- .arch-install/git-config.sh
+++
@@ -1,4 +0,0 @@
-
-#### Git config name mail ####
-git config --global user.email "ayberkydn@gmail.com" 
-git config --global user.name "Ayberk Aydin"         
--- .arch-install/handle-dotfiles.sh
+++ .arch-install/handle-dotfiles.sh
@@ -1,4 +1,7 @@
 
+#### Git config name mail ####
+git config --global user.email "ayberkydn@gmail.com" 
+git config --global user.name "Ayberk Aydin"         
 ###################################
 #### handle dotfiles ##############
 ###################################
