190eac95d1bc1d8be5ace47fd26c1d15c088443a
Ayberk Aydin <e187569@metu.edu.tr>
2020-05-04 01:59:58 +0300

    upd

--- .arch-install/install-essential.sh
+++ .arch-install/install-essential.sh
@@ -7,5 +7,5 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 sudo pacman-mirrors --fasttrack ; sudo pacman -Syyu --noconfirm
 
-sudo pacman -S i3 rofi sxhkd zsh yay awesome-terminal-fonts gvim neovim gtop py3status nitrogen feh base base-devel termite compton flameshot ttf-font-awesome chromium noto-fonts-emoji redshift cmake python-pip neofetch nmap --noconfirm
+sudo pacman -S i3 rofi sxhkd zsh yay awesome-terminal-fonts gvim neovim gtop py3status nitrogen feh base base-devel termite compton flameshot ttf-font-awesome chromium noto-fonts-emoji redshift cmake python-pip neofetch nmap xclip xsel --noconfirm
 
