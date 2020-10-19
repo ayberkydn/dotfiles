6eb772df7f36e9577aad04f57fe9a54e2c95f169
Ayberk Aydin <>
2020-10-19 16:06:38 +0300

    split scripts and dots(maybe)

--- .scripts/install/install-essential.sh
+++ .scripts/install/install-essential.sh
@@ -1,11 +1,14 @@
+## > set mirrors
 sudo pacman-mirrors -c Germany ;sudo pacman -Syu --noconfirm
 
+## > zsh 
 sudo pacman -S zsh --noconfirm
 
-##### OHMYZSH ######
+## > ohmyzsh 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+## > powerlevel10 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 
-sudo pacman -S yay base base-devel chromium cmake awesome-terminal-fonts xclip xsel neovim fzf alacritty tmux --noconfirm
+sudo pacman -S yay base base-devel chromium cmake xclip xsel neovim fzf alacritty tmux tmuxinator --noconfirm
 
---
+++ .scripts/install/other-settings.sh
@@ -0,0 +1,4 @@
+git config --global user.email "ayberkydn@gmail.com" 
+git config --global user.name "Ayberk Aydin"         
+
+dconf load / < dconf-mate-backup
