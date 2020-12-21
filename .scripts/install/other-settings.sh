82c79ce8526491504298689db45bc8de961ab540
Ayberk Aydin <>
2020-12-21 14:07:52 +0300

    upd

--- .scripts/install/other-settings.sh
+++ .scripts/install/other-settings.sh
@@ -1,4 +1,12 @@
+
+# git
 git config --global user.email "ayberkydn@gmail.com" 
 git config --global user.name "Ayberk Aydin"         
 
-dconf load / < dconf-mate-backup
+
+# use cache
+git config --global credential.helper cache
+
+#cache timeout
+git config --global credential.helper 'cache --timeout=3600'
+
