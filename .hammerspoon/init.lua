ae2428552560423245439c41206bbc8b0ba4ddcd
Ayberk Aydin <ayberkydn@gmail.com>
2023-01-03 12:40:41 +0300

    upd

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,11 @@
+hs.loadSpoon("SpoonInstall")
+
+-- spoon.SpoonInstall.repos.other = {
+--     url = "https://github.com/nathancahill/Anycomplete",
+--     desc = "anycomplete repo",
+-- }
+
+spoon.SpoonInstall:andUse("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
+
+print("init.lua loadsdasded")
