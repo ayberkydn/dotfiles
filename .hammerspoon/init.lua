3cca2a53ffe8ca271971dcf367985de87a9f153e
Ayberk Aydin <ayberkydn@gmail.com>
2023-01-26 16:53:42 +0300

    upd

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,11 +1,29 @@
 hs.loadSpoon("SpoonInstall")
 
--- spoon.SpoonInstall.repos.other = {
---     url = "https://github.com/nathancahill/Anycomplete",
---     desc = "anycomplete repo",
--- }
+spoon.SpoonInstall.repos.paperWM = {
+    url = "https://github.com/mogenson/PaperWM.spoon",
+    desc = "paperWM repo",
+}
 
-spoon.SpoonInstall:andUse("ReloadConfiguration")
+hyper = {"cmd","alt","ctrl"}
+Install = spoon.SpoonInstall
+
+Install:andUse("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
+
+
+Install:andUse("FadeLogo",
+               {
+                 config = {
+                   default_run = 1.0,
+                   zoom=false,
+                   fade_in_time=0.3,
+                   fade_out_time=0.3,
+                   run_time=0.5,
+                 },
+                 start = true
+               }
+)
+
 
 print("init.lua loaded")
