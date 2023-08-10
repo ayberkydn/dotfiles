9257f11bb7ca1958cc34a3a38fba3d4898feb108
Ayberk Aydın <ayberkydn@gmail.com>
2023-08-10 18:16:08 +0300

    upd

--- .config/nix/default.nix
+++ .config/nix/default.nix
@@ -30,9 +30,11 @@
     bitwarden-cli
     python3
     anydesk
-    librespot
-    pulseaudio
-    ncspot
+    # pipewire
+    # easyeffects
+    # librespot
+    # pulseaudio
+    pulseeffects-legacy
   ];
 
   # systemd.user.services.librespotDaemon = {
@@ -53,7 +55,7 @@
   # boot.blacklistedKernelModules = [ "snd_pcsp" ];
 
   services.spotifyd = {
-    enable = true;
+    enable = false;
     settings = { 
       global = {
         bitrate = 320;
@@ -75,19 +77,21 @@
   hardware.pulseaudio.support32Bit = true;
   hardware.pulseaudio.package = pkgs.pulseaudioFull;
   nixpkgs.config.pulseaudio = true;
+  programs.dconf.enable = true;
   security.rtkit.enable = true;
-  # services.pipewire = {
-  #   enable = false;
-  #   alsa.enable = true;
-  #   alsa.support32Bit = true;
-  #   pulse.enable = true;
-  #   # If you want to use JACK applications, uncomment this
-  #   #jack.enable = true;
-  #
-  #   # use the example session manager (no others are packaged yet so this is enabled by default,
-  #   # no need to redefine it in your config for now)
-  #   #media-session.enable = true;
-  # };
+  #services.pulseeffects.enable = true;
+  
+  services.pipewire = {
+    enable = false;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
 
   # services.librespot.enable = true;
   services.openssh.enable = true;
