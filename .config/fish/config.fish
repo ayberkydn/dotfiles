6bb9533d5cf0d909741962cff1d23e6689e426e9
installer <installer@gmail.com>
2025-08-08 03:04:19 +0300

    upd

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,35 +1,20 @@
+fish_add_path /opt/homebrew/bin/
+
 # aliases
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias dots='dotfiles commit -am"upd"; dotfiles push'
 
 alias shconfig='nvim ~/.config/fish/config.fish'
-alias xconfig='nvim ~/.xprofile'
-alias vimconfig='vim ~/.vimrc'
 alias tmuxconfig='nvim ~/.tmux.conf'
-alias vconfig='nvim ~/.config/nvim/*.vim'
+alias vimconfig='nvim ~/.config/nvim/*.vim'
 alias tconfig='nvim ~/.config/alacritty/alacritty.yml'
-alias rconfig='nvim ~/.config/ranger/rc.conf'
-alias jcconfig="nvim ~/.jupyter/jupyter_console_config.py"
-
-alias cddocker="cd ~/.dockerfiles/"
-alias cdtmuxinator='cd ~/.config/tmuxinator'
 
 alias cl='clear'
 alias open='xdg-open'
 
-alias vpndown="openvpn3 session-manage --disconnect --config ~/.config/vpn/client.ovpn"
-alias vpnup="openvpn3 session-start --config ~/.config/vpn/client.ovpn"
-
-
-
-
 
 #abbreviations
 abbr --add oc opencode
-
-
-#conda initialize >>>
-#eval /home/ayb/.local/conda/bin/conda "shell.fish" "hook" $argv | source
 
 #remove greeting
 set fish_greeting
@@ -53,13 +38,4 @@ end
 #disable right prompt
 function fish_right_prompt; end
 
-
-##variables
-#set DATA_PATH /home/ayb/Documents/data
-
-
-## >>> conda initialize >>>
-## !! Contents within this block are managed by 'conda init' !!
-#eval /home/ayb/.local/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-## <<< conda initialize <<<
 
